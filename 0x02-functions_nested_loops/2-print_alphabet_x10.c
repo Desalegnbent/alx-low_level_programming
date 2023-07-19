771f5a564c5d4ce703ec5f2b8efc954c22556057
Desalegnbent <desalegnbenti@gmail.com>
2023-07-19 08:30:13 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,13 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - Print lowercase alphabet 10 times
  */
+
 void print_alphabet_x10(void)
 {
 	char num, letter;
+
 	for (num = 0; num <= 9; num++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
@@ -14,3 +17,4 @@ void print_alphabet_x10(void)
 		_putchar('\n');
 	}
 }
+
